70faf688185043ccd4f97380b8b229a2608bf447
Philip Diessner <philip@silpion-ai-lab.com>
2020-12-03 12:06:51 +0100

    use regex for day2

--- src/day2.rs
+++ src/day2.rs
@@ -1,16 +1,20 @@
 use crate::util;
 use std::ops::Range;
+use regex;
 
-fn split_to_vec<'a>(inp: &'a str, delim: & str) -> Vec<&'a str> {
-    inp.split(delim).collect::<Vec<_>>()
+fn parse_line(inp: & str) -> regex::Captures{
+    lazy_static! {
+    static ref RE: regex::Regex = regex::Regex::new(r"^(\d+)-(\d+) (\w): (\w+)$").unwrap();
+    }
+    if let Some(caps) = RE.captures_iter(inp).next() {
+        caps
+    } else { panic!() }
 }
 
-fn valid_pass(policy: &str, pass: &str) -> bool{
-    let a= split_to_vec(policy, " ");
-    let c = pass.matches(a[1]).count() as u32;
-    let d = split_to_vec(a[0], "-");
-    if let Ok(start) = d[0].parse::<u32>() {
-        if let Ok(end) = d[1].parse::<u32>() {
+fn valid_pass(policy: (&str, &str, &str), pass: &str) -> bool{
+    let c = pass.matches(policy.2).count() as u32;
+    if let Ok(start) = policy.0.parse::<u32>() {
+        if let Ok(end) = policy.1.parse::<u32>() {
             let range: Range<u32> = Range{ start, end: end+1};
     range.contains(&c)
         } else { panic!() }
@@ -18,13 +22,11 @@ fn valid_pass(policy: &str, pass: &str) -> bool{
     }else { panic!() }
 }
 
-fn valid_pass_two(policy: &str, pass: &str) -> bool{
-    let a= split_to_vec(policy, " ");
-    let ch : Vec<char> = a[1].chars().collect();
-    let d = split_to_vec(a[0], "-");
+fn valid_pass_two(policy: (&str, &str, &str), pass: &str) -> bool{
+    let ch : Vec<char> = policy.2.chars().collect();
     let pass_vec: Vec<char> = pass.chars().collect();
-    if let Ok(first) = d[0].parse::<usize>(){
-        if let Ok(second) = d[1].parse::<usize>(){
+    if let Ok(first) = policy.0.parse::<usize>(){
+        if let Ok(second) = policy.1.parse::<usize>(){
                 (pass_vec[first - 1] == ch[0] || pass_vec[second - 1] == ch[0]) &&
                     (pass_vec[first - 1] != pass_vec[second - 1])
         } else { panic!() }
@@ -37,8 +39,9 @@ pub fn part1(filename: &str) {
         let mut res = 0;
         for line in lines {
             if let Ok(inp) = line{
-                let split = split_to_vec(inp.as_str(), ": ");
-                res += valid_pass(split[0], split[1]) as u32;
+                let caps= parse_line(inp.as_str());
+                let (pol, pw) = ((&caps[1], &caps[2], &caps[3]), &caps[4]);
+                res += valid_pass(pol, pw) as u32;
 
             }
         }
@@ -52,8 +55,9 @@ pub fn part2(filename: &str) {
         let mut res = 0;
         for line in lines {
             if let Ok(inp) = line{
-                let split = split_to_vec(inp.as_str(), ": ");
-                res += valid_pass_two(split[0], split[1]) as u32;
+                let caps = parse_line(inp.as_str());
+                let (pol, pw) = ((&caps[1], &caps[2], &caps[3]), &caps[4]);
+                res += valid_pass_two(pol, pw) as u32;
             }
         }
         println!("{}",res)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[macro_use] extern crate lazy_static;
 mod util;
 mod day1;
 mod day2;
