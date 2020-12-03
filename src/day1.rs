97870c54185928a6837d1ebd2769ddc57e1d3bd1
Philip Diessner <philip@silpion-ai-lab.com>
2020-12-03 16:01:05 +0100

    use unwrap for Result

--- src/day1.rs
+++ src/day1.rs
@@ -3,45 +3,40 @@ use crate::util;
 
 pub fn part1(filename: &str) {
     let mut done: HashMap<u32, u32> = HashMap::new();
-    if let Ok(lines) = util::read_lines(filename) {
+    let lines = util::read_lines(filename).unwrap();
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
-            if let Ok(ip) = line {
-                if let Ok(trim) = ip.trim().parse::<u32>() {
-                    let rem = 2020 - trim;
-                    if !done.contains_key(&trim) {
-                        done.insert(rem, trim);
-                    } else {
-                        println!("first star: {}", trim * rem);
-                    }
-                }
+            let ip = line.unwrap();
+            let trim = ip.trim().parse::<u32>().unwrap();
+            let rem = 2020 - trim;
+            if !done.contains_key(&trim) {
+                done.insert(rem, trim);
+            } else {
+                println!("first star: {}", trim * rem);
             }
+
         }
-    }
 }
 
 pub fn part2(filename: &str) {
     let mut single: HashMap<u32, u32> = HashMap::new();
     let mut double: HashMap<u32, (u32, u32)> = HashMap::new();
-    if let Ok(lines) = util::read_lines(filename) {
+    let lines = util::read_lines(filename).unwrap();
         // Consumes the iterator, returns an (Optional) String
-        for line in lines {
-            if let Ok(ip) = line {
-                if let Ok(trim) = ip.trim().parse::<u32>() {
-                    let rem = 2020 - trim;
-                    if !double.contains_key(&trim) {
-                        for (&k, &v) in &single {
-                            if trim < k {
-                                double.insert(2020 - v - trim, (trim, v));
-                            }
-                        }
-                        single.insert(rem, trim);
-                    } else {
-                        if let Some((a, b)) = double.get(&trim) {
-                            println!("second star: {}", trim * a * b);
-                        }
-                    }
+    for line in lines {
+        let ip = line.unwrap();
+        let trim = ip.trim().parse::<u32>().unwrap();
+        let rem = 2020 - trim;
+        if !double.contains_key(&trim) {
+            for (&k, &v) in &single {
+                if trim < k {
+                    double.insert(2020 - v - trim, (trim, v));
                 }
+            }
+            single.insert(rem, trim);
+        } else {
+            if let Some((a, b)) = double.get(&trim) {
+                println!("second star: {}", trim * a * b);
             }
         }
     }
--- src/day2.rs
+++ src/day2.rs
@@ -2,7 +2,7 @@ use crate::util;
 use std::ops::Range;
 use regex;
 
-fn parse_line(inp: & str) -> regex::Captures{
+fn parse_line(inp: &str) -> regex::Captures {
     lazy_static! {
     static ref RE: regex::Regex = regex::Regex::new(r"^(\d+)-(\d+) (\w): (\w+)$").unwrap();
     }
@@ -11,26 +11,21 @@ fn parse_line(inp: & str) -> regex::Captures{
     } else { panic!() }
 }
 
-fn valid_pass(policy: (&str, &str, &str), pass: &str) -> bool{
+fn valid_pass(policy: (&str, &str, &str), pass: &str) -> bool {
     let c = pass.matches(policy.2).count() as u32;
-    if let Ok(start) = policy.0.parse::<u32>() {
-        if let Ok(end) = policy.1.parse::<u32>() {
-            let range: Range<u32> = Range{ start, end: end+1};
+    let start = policy.0.parse::<u32>().unwrap();
+    let end = policy.1.parse::<u32>().unwrap();
+    let range: Range<u32> = Range { start, end: end + 1 };
     range.contains(&c)
-        } else { panic!() }
-
-    }else { panic!() }
 }
 
-fn valid_pass_two(policy: (&str, &str, &str), pass: &str) -> bool{
-    let ch : Vec<char> = policy.2.chars().collect();
+fn valid_pass_two(policy: (&str, &str, &str), pass: &str) -> bool {
+    let ch: Vec<char> = policy.2.chars().collect();
     let pass_vec: Vec<char> = pass.chars().collect();
-    if let Ok(first) = policy.0.parse::<usize>(){
-        if let Ok(second) = policy.1.parse::<usize>(){
-                (pass_vec[first - 1] == ch[0] || pass_vec[second - 1] == ch[0]) &&
-                    (pass_vec[first - 1] != pass_vec[second - 1])
-        } else { panic!() }
-    } else { panic!() }
+    let first = policy.0.parse::<usize>().unwrap();
+    let second = policy.1.parse::<usize>().unwrap();
+    (pass_vec[first - 1] == ch[0] || pass_vec[second - 1] == ch[0]) &&
+        (pass_vec[first - 1] != pass_vec[second - 1])
 }
 
 pub fn part1(filename: &str) {
@@ -38,14 +33,13 @@ pub fn part1(filename: &str) {
         // Consumes the iterator, returns an (Optional) String
         let mut res = 0;
         for line in lines {
-            if let Ok(inp) = line{
-                let caps= parse_line(inp.as_str());
-                let (pol, pw) = ((&caps[1], &caps[2], &caps[3]), &caps[4]);
-                res += valid_pass(pol, pw) as u32;
-
-            }
+            let inp = line.unwrap();
+            let caps = parse_line(inp.as_str());
+            let (pol, pw) = ((&caps[1], &caps[2], &caps[3]), &caps[4]);
+            res += valid_pass(pol, pw) as u32;
         }
-        println!("{}",res)
+
+        println!("{}", res)
     }
 }
 
@@ -54,12 +48,12 @@ pub fn part2(filename: &str) {
         // Consumes the iterator, returns an (Optional) String
         let mut res = 0;
         for line in lines {
-            if let Ok(inp) = line{
-                let caps = parse_line(inp.as_str());
-                let (pol, pw) = ((&caps[1], &caps[2], &caps[3]), &caps[4]);
-                res += valid_pass_two(pol, pw) as u32;
-            }
+            let inp = line.unwrap()
+            let caps = parse_line(inp.as_str());
+            let (pol, pw) = ((&caps[1], &caps[2], &caps[3]), &caps[4]);
+            res += valid_pass_two(pol, pw) as u32;
         }
-        println!("{}",res)
+
+        println!("{}", res)
     }
 }
