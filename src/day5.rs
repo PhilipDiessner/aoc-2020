5383bfcda1361ad4f3ae0c68c87ceba138ba5c73
Philip Diessner <philip@silpion-ai-lab.com>
2020-12-06 23:55:14 +0100

    day5 refactor

--- src/day5.rs
+++ src/day5.rs
@@ -3,38 +3,31 @@ use crate::util;
 pub fn part1(filename: &str) {
     let input = util::filename_to_string(filename).unwrap();
     let passes = util::read_map(input.as_str());
-    let mut ids:Vec<u32>  = Vec::new();
+    let mut ids: Vec<u32> = Vec::new();
     for pass in passes {
         let mut row = String::new();
-        let mut col = String::new();
-        for fb in pass[..7].to_vec() {
+        for fb in pass.to_vec() {
             match fb {
                 'F' => row += "0",
                 'B' => row += "1",
+                'L' => row += "0",
+                'R' => row += "1",
                 _ => panic!()
             }
         }
-        for fb in pass[7..].to_vec() {
-            match fb {
-                'L' => col += "0",
-                'R' => col += "1",
-                _ => panic!()
-            }
-        }
-        ids.push(u32::from_str_radix(row.as_str(),2).unwrap()*8+
-            u32::from_str_radix(col.as_str(),2).unwrap());
+        ids.push(u32::from_str_radix(row.as_str(), 2).unwrap());
     }
     ids.sort();
     let max = ids.iter().max().unwrap().clone();
-    let mut start  = ids[0];
+    let mut start = ids[0];
     let mut spot = start;
     for id in ids {
-        if start == id { start += 1 } else { spot = start; break; }
+        if start == id { start += 1 } else {
+            spot = start;
+            break;
+        }
     }
-    println!("{}, {}",max, spot);
+    println!("{}, {}", max, spot);
 }
 
-pub fn part2(filename: &str) {
-
-
-}
+pub fn part2(filename: &str) {}
